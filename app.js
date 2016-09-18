975c8ae4f7de31d1d9921228543fb3f3d7529b33
masi <mdmasiuddin43@gmail.com>
2016-09-17 23:50:22 -0500

    adding ap.js which calculate the value of characters

---
+++ app.js
@@ -0,0 +1,28 @@
+var student = {
+  name:"",
+  type:"student"
+};
+
+document.addEventListener('DOMContentLoaded', contentLoaded);
+
+function contentLoaded(event){
+  document.getElementById("name").addEventListener('keyup', keyup);
+}
+
+function keyup(event) {
+  calculateNumericOutput();
+}
+
+function calculateNumericOutput() {
+
+  student.name= document.getElementById('name').value;
+  var total = 0;
+
+  for(var i=0; i<student.name.length; i++) {
+    total += student.name.charCodeAt(i);
+  }
+
+  var output = total;
+  document.getElementById('count').innerText = output;
+
+}
